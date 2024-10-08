d90520c8e2021ae0a63bfce57980f2141a45b17a
DevonVmontclair06 <volped3@montclair.edu>
2024-10-08 22:05:05 +0000

    Final project foods

--- Assignment.py
+++ Assignment.py
@@ -1,10 +1,10 @@
-hypothetical_foods = set('sandwitch','cake','apple','almonds')
+hypothetical_foods = set(("sandwitch" , "cake" , "apple" , "almonds"))
 
 food_1 = input()
 food_2 = input()
 food_3 = input()
 
-user_foods = set(food_1,food_2,food_3)
+user_foods = set((food_1,food_2,food_3))
 
 print('Enter your first favorite food:', food_1)
 print('Enter your second favorite food:', food_2)
@@ -12,7 +12,7 @@ print('Enter your third favorite food:', food_3)
 
 
 
-if hypothetical_foods=user_foods:
-    print('Our common food(s):',)
+if set.intersection(user_foods,hypothetical_foods):
+    print('Our common food(s):' , set.intersection(user_foods,hypothetical_foods))
 else:
-    print('We have no common foods')
+    print('We have no common foods.')
